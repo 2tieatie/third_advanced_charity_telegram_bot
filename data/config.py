bb41533ebea479ce7343634aad6bec510f2b6b4d
2tieatie <103947853+2tieatie@users.noreply.github.com>
2023-03-09 19:28:18 +0200

    Add files via upload

---
+++ data/config.py
@@ -0,0 +1,15 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = str(os.getenv('BOT_TOKEN'))
+
+admins_id = [
+    int("admins_id")
+]
+IP = os.getenv('ip')
+PGUSER = str(os.getenv('PGUSER'))
+PGPASSWORD = str(os.getenv('PGPASSWORD'))
+DATABASE = str(os.getenv('DATABASE'))
+POSTGRES_URI = f'postgresql://{PGUSER}:{PGPASSWORD}@{IP}/{DATABASE}'
